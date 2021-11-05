7037169c8f156de27e600a201cbcace2f8f4bd6d
2swamped4u <jvrjsanchez@gmail.com>
2021-11-04 20:25:25 -0400

    IIFE example

--- intro/teste.js
+++ intro/teste.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 (function() {
     var x = 0;
     var y = 9;
     console.log( x + y );
 })();
-=======
-var x = 100;
-var y = 9;
-
-function sum() {
-    return x + y;
-}
-console.log(sum())
->>>>>>> 22c9a8e399856b69eba9e3729cefa892e282061f
